54c574f0fc6268dc24c95bc2ccbd0e9a05361a51
Dhinesh <dhineshkumar.rajendran@ust.com>
2021-10-02 19:05:17 +0530

    Added a stream program to  Find highest and lowest paid employees using Streams

---
+++ src/streams/MapDemo1.java
@@ -0,0 +1,14 @@
+package streams;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MapDemo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> words = Arrays.asList("Java8", "Lambdas", "In", "Action");
+//		List<Integer> wordLengths = words.stream().map(String::length).collect(toL);
+	}
+
+}
